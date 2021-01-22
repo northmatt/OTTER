a21a451905e58a359e5bc28a314730632251449d
Rob Savaglio <robert.savaglio@ontariotechu.net>
2021-01-22 13:59:50 -0500

    Starter for GameSound

--- samples/GameSound/SampleStarter/src/Game.cpp
+++ samples/GameSound/SampleStarter/src/Game.cpp
@@ -12,21 +12,7 @@ float gameTime;
 
 void Init()
 {
-	gameTime = 0;
-
-	AudioEngine& audioEngine = AudioEngine::Instance();
-
-	// Init FMOD
-	audioEngine.Init();
-
-	// Load a sound bank
-	audioEngine.LoadBank("Master");
-
-	// Create a music event instance									 // Right-click your event in fmod Studio -> Copy GUID
-	AudioEvent& music = AudioEngine::Instance().CreateEvent("UniqueName", "{b56cb9d2-1d47-4099-b80e-7d257b99a823}");
-
-	// Play the music event
-	music.Play();
+	
 }
 
 //------------------------------------------------------------------------
@@ -38,32 +24,14 @@ void Update(float deltaTime)
 	// Increment game time
 	gameTime += deltaTime;
 
-	// Get a ref to the engine
-	AudioEngine& audioEngine = AudioEngine::Instance();
-
-	// Get a ref to the music event
-	AudioEvent& music = audioEngine.GetEvent("UniqueName");
-
-	// Get listener
-	AudioListener& listener = audioEngine.GetListener();
 	
 	
 	// After 5 seconds go underwater
 	if (gameTime > 4.0f)
 	{
-		music.SetPosition(glm::vec3(10, 0, 0));
+		// Do something
 	}
 
-	// After 10 second slow down
-	if (gameTime > 10.0f)
-	{
-		audioEngine.SetGlobalParameter("Timewarp", 0);
-		music.SetParameter("Underwater", 1);
-	}
-
-
-	// Update fmod
-	audioEngine.Update();
 
 }
 
@@ -82,6 +50,5 @@ void Render()
 //------------------------------------------------------------------------
 void Shutdown()
 {
-	AudioEngine::Instance().Shutdown();
 
 }
